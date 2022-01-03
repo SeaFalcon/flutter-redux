79b1b7eb22f434f8d129073d6b830e67688d9f2e
SeaFalcon <seafalcon323@gmail.com>
2022-01-04 01:02:52 +0900

    nested view model -> single view model

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -9,93 +9,84 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<AppState, _ColorViewModel>(
-      converter: (store) => _ColorViewModel(
-        state: store.state.colorState,
+    return StoreConnector<AppState, _ColorCounterViewModel>(
+      converter: (store) => _ColorCounterViewModel(
+        state: store.state,
         onColorChanged: () {
           print('onColorChanged!');
           store.dispatch(ChangeColorAction());
         },
+        onCounterIncremented: () {
+          print('onCounterIncremented!!');
+          store.dispatch(IncrementAction(color: store.state.colorState.color));
+        },
+        onCounterDecremented: () {
+          print('onCounterDecremented!!');
+          store.dispatch(DecrementAction(color: store.state.colorState.color));
+        },
       ),
-      builder: (BuildContext context, _ColorViewModel colorVM) {
-        return StoreConnector<AppState, _CounterViewModel>(
-          converter: (store) => _CounterViewModel(
-            state: store.state.counterState,
-            onCounterIncremented: () {
-              print('onCounterIncremented!!');
-              store.dispatch(IncrementAction(color: colorVM.state.color));
-            },
-            onCounterDecremented: () {
-              print('onCounterDecremented!!');
-              store.dispatch(DecrementAction(color: colorVM.state.color));
-            },
+      builder: (BuildContext context, _ColorCounterViewModel colorCounterVM) {
+        return Scaffold(
+          backgroundColor: colorCounterVM.state.colorState.color,
+          appBar: AppBar(
+            backgroundColor: Colors.blueAccent,
+            actions: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Posts',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
           ),
-          builder: (BuildContext context, _CounterViewModel counterVM) {
-            return Scaffold(
-              backgroundColor: colorVM.state.color,
-              appBar: AppBar(
-                backgroundColor: Colors.blueAccent,
-                actions: [
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Posts',
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                    ),
+          body: SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  colorCounterVM.state.counterState.counter.toString(),
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.w400),
+                ),
+                ElevatedButton(
+                  onPressed: colorCounterVM.onColorChanged,
+                  child: Text(
+                    'Change Color',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+          ),
+          floatingActionButton: Padding(
+            padding: EdgeInsets.only(
+              right: 15,
+              bottom: 15,
+            ),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FloatingActionButton(
+                    heroTag: 'btn1',
+                    onPressed: colorCounterVM.onCounterIncremented,
+                    child: Icon(Icons.add),
+                  ),
+                  SizedBox(width: 5),
+                  FloatingActionButton(
+                    heroTag: 'btn2',
+                    onPressed: colorCounterVM.onCounterDecremented,
+                    child: Icon(Icons.remove),
                   ),
                 ],
               ),
-              body: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      counterVM.state.counter.toString(),
-                      // '${counterVM.state.counter}',
-                      style:
-                          TextStyle(fontSize: 50, fontWeight: FontWeight.w400),
-                    ),
-                    ElevatedButton(
-                      onPressed: colorVM.onColorChanged,
-                      child: Text(
-                        'Change Color',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              floatingActionButton: Padding(
-                padding: EdgeInsets.only(
-                  right: 15,
-                  bottom: 15,
-                ),
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      FloatingActionButton(
-                        heroTag: 'btn1',
-                        onPressed: counterVM.onCounterIncremented,
-                        child: Icon(Icons.add),
-                      ),
-                      SizedBox(width: 5),
-                      FloatingActionButton(
-                        heroTag: 'btn2',
-                        onPressed: counterVM.onCounterDecremented,
-                        child: Icon(Icons.remove),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
+            ),
+          ),
         );
       },
     );
@@ -119,6 +110,19 @@ class _CounterViewModel {
 
   _CounterViewModel({
     required this.state,
+    required this.onCounterIncremented,
+    required this.onCounterDecremented,
+  });
+}
+
+class _ColorCounterViewModel {
+  final AppState state;
+  final void Function() onColorChanged;
+  final void Function() onCounterIncremented;
+  final void Function() onCounterDecremented;
+  _ColorCounterViewModel({
+    required this.state,
+    required this.onColorChanged,
     required this.onCounterIncremented,
     required this.onCounterDecremented,
   });
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
